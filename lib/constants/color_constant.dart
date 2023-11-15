a4fa56202725347e0486d7c472e0b3ada1b1a8ab
ABDULSAMED <samet.1198@gmail.com>
2023-11-15 23:11:24 +0300

    add ✨: add color

---
+++ lib/constants/color_constant.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color whiteteam1 = Colors.white.withOpacity(0.5) ;
+Color whiteteam2 = Colors.white.withOpacity(0.15);
+Color whiteteam3 = Colors.white.withOpacity(0.13);
